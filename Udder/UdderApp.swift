b794e69b2c7257a75e628e66cf97580fee9c156c
davidcode10 <davidedson2030@outlook.com>
2022-08-03 14:21:00 +0200

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by David Santiago on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
